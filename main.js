1d64ec56501f942193ca81ac129230ad4b9b19c1
Roxanna Abigail <rati.bigigigil@gmail.com>
2025-12-27 02:46:48 -0600

    English!! version :D

--- main.js
+++ main.js
@@ -4,15 +4,25 @@ let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
 });
- 
+ if (document.documentElement.lang === "en") {
+  typewriter
+    .pauseFor(2500)
 typewriter
   .pauseFor(2500)
+  .typeString('<span style="color: #fff786"> Digital Artist and Animator </span>')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
+
+ } else {
+  typewriter
+ .pauseFor(2500)
   .typeString('<span style="color: #fff786"> Artista Digital y Animadora </span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
 
-
+ }
   document.addEventListener("DOMContentLoaded", function() {
     const images = document.querySelectorAll('.float-img');
     
@@ -26,4 +36,11 @@ typewriter
         img.style.left = `${randomLeft}%`;
         img.style.animationDelay = `${randomDelay}s`;
     });
+});
+
+document.getElementById('idioma').addEventListener('change', function() {
+    const selectedPage = this.value;
+    if (selectedPage) {
+        window.location.href = selectedPage;
+    }
 });
